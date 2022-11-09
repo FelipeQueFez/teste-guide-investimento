c112080528c64ef526afffa09112c300f8aa1988
FelipeQueFez <felipequefez@gmail.com>
2022-11-08 22:36:19 -0300

    skeleton packages

---
+++ guide_app/android/app/src/main/kotlin/com/guideapp/guide_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.guideapp.guide_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ guide_core/lib/guide_core.dart
@@ -0,0 +1,7 @@
+library guide_core;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
